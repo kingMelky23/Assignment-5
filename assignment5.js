79f31833b023e6a82897121c0243b65afa678387
king <tettehmel@gmail.com>
2020-06-02 08:06:31 -0400

    fixed error, grid can now update

--- assignment5.js
+++ assignment5.js
@@ -9,7 +9,23 @@ let buttonSubmit = document.getElementById("button-submit");
 buttonSubmit.addEventListener("click", function(event){
     if(rows !== 0 && columns !== 0)
     {
-    
+        console.log("row",rows)
+        let newRows = parseFloat(document.getElementById("addRows").value) || 0;
+        let newColumns = parseFloat(document.getElementById("addColumns").value) || 0;
+        let newRemoveRows = document.getElementById("removeRows").value || 0;
+        let newRemoveColumns = document.getElementById("removeColumns").value || 0;
+
+        //let totalRows = (parseFloat(newRows) + parseFloat(rows)) - (newRemoveRows ? parseFloat(newRemoveRows) : 0);
+        //console.log("newRem",newRemoveRows)
+        //let totalColumns = (parseFloat(newColumns) + parseFloat(columns)) - (newRemoveColumns ? parseFloat(newRemoveColumns) : 0);
+
+        console.log("newRows",newRows);
+        console.log("newColumns",newColumns);
+        //console.log("total rows",totalRows);
+
+        makeRows(newRows,newColumns)
+
+
     }
     else{
         addRows = document.getElementById("addRows").value;
@@ -20,8 +36,8 @@ buttonSubmit.addEventListener("click", function(event){
         if(addRows <= removeRows || addColumns <= removeColumns)
         {
             alert("Nothing to remove!");
-            document.getElementById("removeRows").value = null;
-            document.getElementById("removeColumns").value = null;
+            document.getElementById("removeRows").value = 0;
+            document.getElementById("removeColumns").value = 0;
             removeRows = 0;
             removeColumns = 0;
             rows = 0;
@@ -39,4 +55,18 @@ buttonSubmit.addEventListener("click", function(event){
     }
     
     
+    
     });
+
+    const container = document.getElementById("grid-container");
+
+    function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    console.log("tot",rows*cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        //cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+    };
