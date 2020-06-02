1697edbb702d85c2449b2988d65df44940d32981
king <tettehmel@gmail.com>
2020-06-01 21:50:53 -0400

    creating grid

--- assignment5.js
+++ assignment5.js
@@ -6,42 +6,37 @@ let rows = 0;
 let columns = 0;
 let buttonSubmit = document.getElementById("button-submit");
 
-
 buttonSubmit.addEventListener("click", function(event){
-addRows = document.getElementById("addRows").value;
-addColumns = document.getElementById("addColumns").value;
-removeRows = document.getElementById("removeRows").value;
-removeColumns = document.getElementById("removeColumns").value;
-
-rows = rows + addRows;
-rows = rows - removeRows;
-
-columns = columns + addColumns;
-columns = columns - removeColumns;
-
-
-
-if(addRows == 1){
-    alert("it worked");
-}
-else{
-    alert("it didnt")
-}
-});
-
-
-let buttonColorUncolored = document.getElementById("colorUncolored");
-buttonColorUncolored.addEventListener("click", function(event){
-
-});
-
-
-let buttonColorAll = document.getElementById("colorAll");
-buttonColorAll.addEventListener("click", function(event){
-
-});
-
-let buttonClearAll = document.getElementById("clearAll");
-buttonClearAll.addEventListener("click", function(event){
-
-});
+    if(rows !== 0 && columns !== 0)
+    {
+    
+    }
+    else{
+        addRows = document.getElementById("addRows").value;
+        addColumns = document.getElementById("addColumns").value;
+        removeRows = document.getElementById("removeRows").value;
+        removeColumns = document.getElementById("removeColumns").value;
+    
+        if(addRows <= removeRows || addColumns <= removeColumns)
+        {
+            alert("Nothing to remove!");
+            document.getElementById("removeRows").value = null;
+            document.getElementById("removeColumns").value = null;
+            removeRows = 0;
+            removeColumns = 0;
+            rows = 0;
+            columns = 0;
+        }
+        else{
+            rows = addRows - removeRows;
+            columns = addColumns - removeColumns;
+    
+    
+            console.log(rows + columns);
+            makeRows(rows, columns);
+        }
+    
+    }
+    
+    
+    });
